7357d0e98dae7db39c54af1adee99da36d49e32c
fallangelli <qwer123$>
2015-12-13 01:01:48 +0800

    server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,10 @@
 angular.module('bibimovie.controllers', [])
 
   .controller('HomeCtrl', function ($scope, $http, ApiEndpoint, HomeService) {
-    var url = ApiEndpoint.server_url + "hotMovies?cityId=1";
+    var url = ApiEndpoint.server_url + "hotMovies?cityId=2861";
     $http.get(url)
       .success(function (data) {
-        $scope.movies = data;
+        var movies = angular.fromJson(data)
+        $scope.movieList = movies;
       })
   })
